08bc0d1130bbfd39b58f93f3f5e1bb849c3f95a1
HIkkigayaH <protimpal0@gmail.com>
2020-09-25 23:20:26 +0530

    'desc'

--- sketch.js
+++ sketch.js
@@ -2,8 +2,9 @@ var xts = []
 var t = 0
 var del_t = 0.1
 var slider
+var xOffset = 100
 function setup() {
-    createCanvas(displayWidth, 400);
+    createCanvas(displayWidth, xOffset0);
     frameRate(100)
     fill(0)
     textSize(16)
@@ -21,17 +22,17 @@ function draw() {
     }
     xt = (1 / 2) - xt
     xts.unshift(xt)
-    line(40, 50, 40, 300)
-    line(40, 300, 450, 300)
+    line(xOffset, 50, xOffset, 300)
+    line(xOffset, 300, 850, 300)
     text('t', 250, 320)
     text('x(t)', 10, 200)
     text(`Number of sinusoids used: ${slider.value()}`, 350, 50)
     text("Fourier Series Expansion of Saw-Tooth Signal", width/2-50, height-20)
     beginShape(LINES)
     for (let i = 0; i < xts.length; i++) {
-        vertex(i + 40, 100 * xts[i] + 200)
+        vertex(i + xOffset, 100 * xts[i] + 200)
     }
     endShape()
-    if (xts.length > 400)
+    if (xts.length > 800)
         xts.pop()
 }
