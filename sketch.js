cf5203bba224e445ab23accf03158f0065c0cbc9
HIkkigayaH <protimpal0@gmail.com>
2020-09-25 23:15:47 +0530

    'desc'

--- sketch.js
+++ sketch.js
@@ -26,6 +26,7 @@ function draw() {
     text('t', 250, 320)
     text('x(t)', 10, 200)
     text(`Number of sinusoids used: ${slider.value()}`, 350, 50)
+    text("Fourier Series Expansion of Saw-Tooth Signal", width/2, height-20)
     beginShape(LINES)
     for (let i = 0; i < xts.length; i++) {
         vertex(i + 40, 100 * xts[i] + 200)
