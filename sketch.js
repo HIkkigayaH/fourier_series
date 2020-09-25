c5b196ccacd0e580ddfc355c2e94f618426202f2
HIkkigayaH <protimpal0@gmail.com>
2020-09-25 21:46:15 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var xts = []
+var t = 0
+var del_t = 0.1
+var slider
+function setup() {
+    createCanvas(700, 400);
+    frameRate(100)
+    fill(0)
+    textSize(16)
+    slider = createSlider(1, 10000, 1000)
+    slider.position(10, 10)
+    slider.style('width', '680px')
+}
+
+function draw() {
+    background(220);
+    t += del_t
+    let xt = 0
+    for (let n = 1; n < slider.value(); n++) {
+        xt += (1 / (n * 3.14)) * sin(n * t)
+    }
+    xt = (1 / 2) - xt
+    xts.unshift(xt)
+    line(40, 50, 40, 300)
+    line(40, 300, 450, 300)
+    text('t', 250, 320)
+    text('x(t)', 10, 200)
+    text(slider.value(), 350, 50)
+    beginShape(LINES)
+    for (let i = 0; i < xts.length; i++) {
+        vertex(i + 40, 100 * xts[i] + 200)
+    }
+    endShape()
+    if (xts.length > 400)
+        xts.pop()
+}
